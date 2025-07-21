28e3dc65b1cac911da2ec5ddfcb4304b9ea2a4c5
CristianGestor <cristian.gerir@gmail.com>
2025-07-21 09:36:35 -0300

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <h1>Gestor de Operações</h1>
+      <p>Bem-vindo à sua nova aplicação!</p>
+    </>
+  )
+}
+
+export default App
