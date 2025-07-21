66e104f2693bcfe999de3f1f9ff0a9a1698c6e1f
CristianGestor <cristian.gerir@gmail.com>
2025-07-21 11:38:17 -0300

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,8 +1,10 @@
 import { createClient } from '@supabase/supabase-js'
 
-// Pega a URL e a Chave Anon que configuramos na Vercel
+// Lê a URL do projeto do arquivo de ambiente da Vercel.
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+
+// Lê a chave pública (anon) do arquivo de ambiente da Vercel.
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
-// Cria e exporta o cliente Supabase para ser usado em outras partes do projeto
+// Cria e exporta o cliente Supabase para ser usado em outros lugares do aplicativo.
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
